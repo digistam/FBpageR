1d957045202efe1259e693588d3bc84f1d73ec5b
digistam <digistam@gmail.com>
2015-02-24 17:06:01 +0100

    bijgewerkt Object_stream.py (csv, likers)

--- facebookobjects.py
+++ facebookobjects.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 objects = [
-440013412805144,    #UpdateMH17
-250183185190629,    #MH17
-477228909079778,    #HerdenkingsdagMH17
-766441713419382,    #HollandforMH17
-455154817946160,    #MH 370 & 17 public group
-308212072681117,    #R.I.P slachtoffers vlucht MH17
-1425326377756453     #MH17 False Flag
+1405126929767971,   #Satudarah Utrecht
+734906803230026,    #Satudarah Southside
+693610654028461,    #No Surrender MC Dark-Side
+222519181134315,    #Satudarah Westside
+592058780856288,    #Bandidos MC
+436398749756617,    #Demons MC Netherlands
+165301590255273,    #Black Sheep MC (2 19 / Crew / Support)
+399361460194405,    #Hells Angels MC SO CAL
+503248919717670,    #HAMC South East
+411929698892809,    #Support Hells Angels Utrecht
+470634159734778,    #Veterans MC Murphy Crew Breda
+100000306901191,    #michael.biezen
+100001262362930,    #rob.vandebiezen
+100006516048361     #klaas.otto.54
 ]
